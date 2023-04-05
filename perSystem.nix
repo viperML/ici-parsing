2b9aeae61cd2e1fafcde1a234662b2c1fbcb6d16
Fernando Ayats <ayatsfer@gmail.com>
2023-04-05 10:53:12 +0000

    testing python

--- perSystem.nix
+++ perSystem.nix
@@ -1,22 +1,11 @@
 {pkgs, ...}: {
   devShells.default = with pkgs;
-    mkShell rec {
-      myPython = python3.withPackages (p: [
-        p.jupyter-core
-      ]);
-
-      env = buildEnv {
-        name = "shell-env";
-        paths = [
-          myPython
-          julia-bin
-        ];
-      };
-
-      packages = [env];
-
-      shellHook = ''
-        ln -Tsf ${env} .venv
-      '';
+    mkShell {
+      packages = [
+        poetry
+      ];
+      # LD_LIBRARY_PATH = lib.makeLibraryPath [
+      #   stdenv.cc.cc
+      # ];
     };
 }
